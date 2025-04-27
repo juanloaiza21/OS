0e2fcbb4469d30dcd09dc88ae8f8f702eba64fa5
juanloaiza21 <juan400reyesloaiza@gmail.com>
2025-04-27 09:05:01 -0500

    left input through cli

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,25 @@ fn read_csv<P: AsRef<Path>>(filename: P) -> Result<Vec<String>, Box<dyn Error>> 
 
 fn main() -> Result<(), Box<dyn Error>> {
     let filename = "/home/jorge/Documents/juan/uni/os/taller1/src/yellow_tripdata_2020-06.csv";
-    let mut array = Vec::new();
-    array = read_csv(filename)?;
+    let array = read_csv(filename)?;
     // print array
     println!("Number of celds: {}", array.len());
-    let mut carlos = Vec::new();
-    for i in 0..carlos.len() {}
+    let mut carlos: Vec<Vec<String>> = Vec::new();
+    let mut j = 0;
+    let mut i = 0;
+    let mut auxArray: Vec<String> = Vec::new();
+    while i < array.len() && j <= 18 {
+        auxArray.push(array[i].to_string());
+        j += 1;
+        i += 1;
+        if j == 18 {
+            carlos.push(auxArray);
+            j = 0;
+            auxArray = Vec::new();
+        }
+    }
+    //TODO input del index por consola
     // print array
-    println!("Data: {}", carlos[0]);
+    println!("Data: {:?}", carlos[1]);
     Ok(())
 }
