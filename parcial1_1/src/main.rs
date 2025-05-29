37897868a5b93b179586d56780e69357bad48ef8
juanloaiza21 <juan400reyesloaiza@gmail.com>
2025-05-28 20:59:06 -0500

    Fixing some logs

--- parcial1_1/src/main.rs
+++ parcial1_1/src/main.rs
@@ -19,7 +19,7 @@ fn handle_client(mut stream: TcpStream) {
 //Entry point
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8080").expect("Failed to bind to address");
-    println!("Server listening on 127.0.0.1:8080");
+    println!("Server listening on 0.0.0.0:8080");
 
     for stream in listener.incoming() {
         match stream {
